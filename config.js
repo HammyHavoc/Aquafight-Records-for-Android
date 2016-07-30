942fef2bb96e91798a7891ca2d12a513ba4431ee
Hammy Havoc <hammy@previousmagazine.com>
2016-07-30 06:29:41 +0100

    Added first build.

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'aquafight-records-android',
+		wp_ws_url : 'https://www.aquafightrecords.com/wp-appkit-api/aquafight-records-android',
+		wp_url : 'https://www.aquafightrecords.com',
+		theme : 'q-android',
+		version : '1.0',
+		app_title : 'Aquafight Records',
+		app_platform : 'android',
+		gmt_offset : -5,
+		debug_mode : 'off',
+		auth_key : '-setw)rpU3uoJ7~jxjOC}!A6_g]>9TXos79AZe uM3M]1R%O4D8-fsv+e2v}Y597',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
